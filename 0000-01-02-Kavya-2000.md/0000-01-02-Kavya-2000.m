c136ac906103312a6ad24db421947c7de636402a
Kavya Matcha <61905658+Kavya-2000@users.noreply.github.com>
2022-03-08 22:36:55 +0530

    Update 0000-01-02-Kavya-2000.m

--- 0000-01-02-Kavya-2000.md/0000-01-02-Kavya-2000.m
+++ 0000-01-02-Kavya-2000.md/0000-01-02-Kavya-2000.m
@@ -4,3 +4,8 @@ title: “Welcome to our second slide!”
 ---
 Your test
 Use the left arrow to go back!
+layout: slide
+title: “Welcome to our second slide!”
+---
+Your test
+Use the left arrow to go back!
